ca2a43a8ba664c158c990210c5fbb4d49f63b0d6
Guillaume DEBUCQUET <gdeb@gdeb.fr>
2026-07-17 08:48:07 +0200

    wip : implémentation DomainError.

--- src/Domain/links/OriginalUrlVo.js
+++ src/Domain/links/OriginalUrlVo.js
@@ -1,19 +1,37 @@
+import DomainError from "../Errors/DomainError.js";
+
 export default class OriginalUrlVo {
     constructor(originalUrl) {
         if (!originalUrl || typeof originalUrl !== 'string') {
-            throw new Error('URL is required and must be a string.');
+            throw new DomainError(
+                'URL is required and must be a string.',
+                'URL_MISSING',
+                'validation'
+            );
         }
         if (originalUrl.length > 2048){
-            throw new Error('URL length cannot exceed 2048 characters.');
+            throw new DomainError(
+                'URL length cannot exceed 2048 characters.',
+                'URL_TOO_LONG',
+                'validation'
+            );
         }
         let parsedUrl; 
         try {
             parsedUrl = new URL(originalUrl);
         } catch (_) {
-            throw new Error('Invalid URL format.');
+            throw new DomainError(
+                'Invalid URL format.',
+                'URL_INVALID',
+                'validation'
+            );
         }
         if (parsedUrl.protocol !== 'http:' && parsedUrl.protocol !== 'https:') {
-            throw new Error('Only http and https protocols are supported.');
+            throw new DomainError(
+                'Only http and https protocols are supported.',
+                'URL_INVALID_PROTOCOL',
+                'validation'
+            );
         }
         this.originalUrl = originalUrl;
         Object.freeze(this);
--- src/Domain/links/ShortCodeVo.js
+++ src/Domain/links/ShortCodeVo.js
@@ -4,6 +4,9 @@ export default class ShortCodeVo {
         if (typeof shortCode !== 'string') {
             throw new Error('shortCode must be a string');
         }
+        if (shortCode && !shortCode.match(/^[a-zA-Z0-9]$/)) {
+            throw new Error('shortCode is composed of invalid characters.');
+        }
         this.#shortCode = shortCode;
         Object.freeze(this);
     }
